847f3ba410befff009b37c6c408a0f60a3ef3575
近藤誠悟 <3th76sk.1156@gmail.com>
2023-03-04 17:58:18 +0900

    third

--- src/main/java/jp/gihyo/projava/tasklist/TaskListApplication.java
+++ src/main/java/jp/gihyo/projava/tasklist/TaskListApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskListApplication {
 
-    //ここがメインクラス
+    //ここがメインクラス　※メインクラスを実行することでアプリケーションが起動する
     public static void main(String[] args) {
         SpringApplication.run(TaskListApplication.class, args);
     }
